222eba008b3f280945161892546159576edc076a
Cao <cao@jaggedverge.com>
2023-01-28 22:51:14 -0500

    Made bootable

--- Source/BootSector/bootloader.asm
+++ Source/BootSector/bootloader.asm
@@ -3,13 +3,23 @@
 ; Cao Smith 2023
 ; Part of CaoOS: An X86-64 Toy System
 
-%define Origin 0X7C00
-; Set Origin of BootLoader in Memory
-; BIOS will put us here, so setup the offset
-[ORG Origin]
+%define Origin 0X7C00          ; Define Origin of BootLoader in Memory
 
-; Config for Real Mode - The default X86-xx start mode
-[BITS 16]
+; Processes assume a 0000:0000 memory address start, requiring manual calculation 
+; every call to find correct segments, since it's unlikely to be loaded there
+; Assembler also provides a global "process" offset
+[ORG Origin]                   ; BIOS will put us here, so setup the offset
 
-boot_loader_neverends: Jmp $		; Tells the Code to just keep loading this instruction. Ad inifinitum.
 
+[BITS 16]                       ; Config for Real Mode - The default X86-xx start mode
+
+boot_loader_neverends: Jmp $	; Tells the Code to just keep loading this instruction. Ad inifinitum.
+
+; Data Delcarations
+boot_msg: DB `\r\n`, 250, 254, `[Cao OS]`,254, 250, `\r\n`,0 ; Start-up Message
+
+
+
+; A boot loader must be in Cylinder 0, Head 0, Sector 1, occupy the entirety & identify as bootloader
+Times 510 - ($ - $$) DB 0x00 ; Pad program to place magic number where expected (Byte 510 to Byte 512)
+boot_code: DW 0xAA55         ; Magic Number - this differentiates bootloader from random files
