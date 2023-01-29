68a50d4784d308c7212726828400843f287c93a7
Cao <cao@jaggedverge.com>
2023-01-29 15:33:42 -0500

    Squashed commit of the following:
    commit ff72b2d843df19e73001e2e86a40e4153c9ddbac
    Author: Cao <cao@jaggedverge.com>
    Date:   Sun Jan 29 15:32:48 2023 -0500
    
        Added new error message if Extensions don't exist on boot medium (IE: Floppy). Added check for floppy boot & skip Extensions Check
    
    commit 1f47b01db6f0a65bd33c3b2a80a756d6f9d39827
    Author: Cao <cao@jaggedverge.com>
    Date:   Sun Jan 29 14:56:53 2023 -0500
    
        Updated ignore
        Added new run steps to create a disk image with bootloader
        Added disk_load function, using EQUate constants to specify memory addresses
    
    commit 6ff2f103c7d9914022c193bf3e12a2dbe30d735b
    Author: Cao <cao@jaggedverge.com>
    Date:   Sat Jan 28 23:16:40 2023 -0500
    
        Added hex printing function
    
    commit af0feebf5c368729c197e65beccecd753a17717d
    Author: Cao <cao@jaggedverge.com>
    Date:   Sat Jan 28 22:59:10 2023 -0500
    
        Added print function
         Print boot message to screen
    
    commit 222eba008b3f280945161892546159576edc076a
    Author: Cao <cao@jaggedverge.com>
    Date:   Sat Jan 28 22:51:14 2023 -0500
    
        Made bootable
    
    commit 737220fd71ed67dffd0d3d143e93c7d63d081661
    Author: Cao <cao@jaggedverge.com>
    Date:   Sat Jan 28 22:36:54 2023 -0500
    
        Updated Ignore
        Restructured Source dir
        Renamed bootloader dir to BootSector, to reflect upcoming changes
        Created rudimentary CMake build (ASM support blows)
        Created Compile convenience script - hope make

---
+++ Source/BootSector/16bit/print_service.asm
@@ -0,0 +1,35 @@
+; BIOS (Real Mode/16bit) based Print Service
+
+; Cao Smith 2023
+; Part of CaoOS: An X86-64 Toy System
+
+
+[BITS 16]       ; Address length will be 16 when invoked (at boot time)
+
+
+; Define the function to print using BIOS
+; BX = argument [String Pointer]
+bios_print:
+
+    Pusha     ; Save the registers to stack
+
+
+    Mov AH, 0x0E     ; Set AH (the HIGH half of AX) to the BIOS teletype services, executed at INT10
+
+
+    ; Loop over the memory address in BX, until null-terminated string
+    .loop:
+        Cmp byte[BX], 0     ; Compare the current byte being pointed to be BX to the null-terminator value
+        Je bios_print_exit
+
+        Mov AL, byte[BX]    ; Put the character of the sting in the low half of AX
+        Int 0x10            ; Call interupt 10h - Parameter is in AH already
+        Inc BX              ; Increment pointer to next char
+
+        Jmp bios_print.loop
+
+bios_print_exit:
+    Popa                    ; Restore the AX & BX register values to pre-print call values
+
+    Ret                     ; Blow this popsicle joint
+
---
+++ Source/BootSector/16bit/printhex_service.asm
@@ -0,0 +1,72 @@
+; BIOS based number printing Service
+;
+; Cao Smith 2023
+; Part of CaoOS: An X86-64 Toy System
+
+; Address length will be 16 when invoked (at boot time)
+[BITS 16]
+
+
+; Define the function to print hex numbers using BIOS
+; BX = Argument [Value Pointer]
+bios_print_hex:
+
+    
+    Pusha           ; Save the registers to the stack
+    
+    Mov AH, 0x0E    ; Set AH (the HIGH half of AX) to the BIOS teletype services, executed at INT10            
+    
+    ; Print Hex Qualifier section
+    Mov AL, '0'     ; Set print param to '0' char
+    INT 0x10        ; Raise interupt to execute BIOS Teletype
+    Mov AL, 'x'     ; Set print param to 'x' char
+    INT 0x10        ;  Raise interupt to execute BIOS Teletype
+  
+    Mov CX, 4       ; Initialize counter
+                    ; BX is 16bits, which is 4 nibbles    
+                    ; Iterate 4 times 
+
+    ; Loop over the value in BX until counter zeroes
+    .loop:
+        
+        Cmp CX, 0                       ; Check if counter is exhausted
+        Je bios_print_hex.hexit         ; Exit if so
+
+        Push BX                         ; Save the progression of BX
+
+        Shr BX, 0x0C                    ; Shift the high nibble to the low nibble
+        
+        
+        Cmp BX, 0x0A                    ; BX >= 10?
+        jge bios_print_hex.high_value   ; Calculate high value
+            ; BX < 10: Calculate low value
+            
+            Mov AL, '0'                 ; Set 0 in AL
+            Add AL, BL                  ; Add lower BX to it
+
+            jmp bios_print_hex.print    ; Go put it on screen
+
+        
+        .high_value:                    ; BX >= 10, Subtract base amount & add to first printable character 
+            Sub BL, 0x0A                ; Cut 10 from Lower BX
+            Mov AL, 'A'                 ; Make Lower AX 'A' Char (96)
+            Add AL, BL                  ; Add Lower BX to lower AX
+        
+        
+        .print:                     ; Print the current nibble to screen device
+            INT 0x10                ; Raise in 10h, invokes Teletype Service
+
+            Pop BX                  ; Restore BX
+            Dec CX                  ; Decrease counter        
+            shl BX, 4               ; Shift out the last nibble
+
+    
+            jmp bios_print_hex.loop ; Back to the loop
+    
+
+    .hexit:                         ; Done here
+        Mov AL, 'h'                 ; Set lower AX to 'h' char (Hex Suffix)
+        INT 0x10                    ; Raise INT, invoke Teletype service
+        Popa                        ; Restore the AX & BX register values to pre-print call values
+        
+        Ret                         ; Blow this popsicle joint
