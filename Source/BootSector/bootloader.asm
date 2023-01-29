af0feebf5c368729c197e65beccecd753a17717d
Cao <cao@jaggedverge.com>
2023-01-28 22:59:10 -0500

    Added print function  Print boot message to screen

--- Source/BootSector/bootloader.asm
+++ Source/BootSector/bootloader.asm
@@ -13,7 +13,16 @@
 
 [BITS 16]                       ; Config for Real Mode - The default X86-xx start mode
 
+Mov BX, boot_msg                ; Save boot message as param to register BX
+call bios_print                 ; Call the print function
+
 boot_loader_neverends: Jmp $	; Tells the Code to just keep loading this instruction. Ad inifinitum.
+
+
+; Declarations
+
+; Inclusivity
+%INCLUDE 'Source/BootSector/16bit/print_service.asm'
 
 ; Data Delcarations
 boot_msg: DB `\r\n`, 250, 254, `[Cao OS]`,254, 250, `\r\n`,0 ; Start-up Message
